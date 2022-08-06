2b8b1aea60beef6b889a8d9aeefb6501af4e121a
jeonga-Son <jjeong0106@gmail.com>
2022-08-06 14:37:01 +0900

    thymleaf에서 form 전송하기

--- src/main/java/com/godcoder/myhome/controller/BoardController.java
+++ src/main/java/com/godcoder/myhome/controller/BoardController.java
@@ -2,12 +2,14 @@ package com.godcoder.myhome.controller;
 
 import com.godcoder.myhome.model.Board;
 import com.godcoder.myhome.repository.BoardRepository;
+import com.godcoder.myhome.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -16,10 +18,41 @@ public class BoardController {
 
     @Autowired
     private BoardRepository boardRepository;
+
+    @Autowired
+    private BoardValidator boardValidator;
+
     @GetMapping("/list")
     public String list(Model model) {
         List<Board> boards = boardRepository.findAll(); //데이터 모두 가져옴.
         model.addAttribute("boards", boards);
         return "board/list";
     }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        //값이 null일 경우 새 board 클래스를 생성해서 타임리프에 넘겨줌.
+        if(id == null) {
+            model.addAttribute("board", new Board());
+        }
+
+        else {
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
+        return "board/form";
+    }
+
+    @PostMapping("/form") //board를 다시 받아온다.
+    public String greetingSubmit(@Valid Board board, BindingResult bindingResult) {
+        boardValidator.validate(board, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "board/form";
+        }
+
+        boardRepository.save(board);
+        return "redirect:/board/list"; //값을 뿌려주지 않고 바로 이동하기 때문에 redirect씀.
+    }
+
 }
--- src/main/java/com/godcoder/myhome/model/Board.java
+++ src/main/java/com/godcoder/myhome/model/Board.java
@@ -4,6 +4,8 @@ package com.godcoder.myhome.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -15,6 +17,8 @@ public class Board {
     @Id //Id가 pk라는 것을 알려주기 위해
     @GeneratedValue(strategy = GenerationType.IDENTITY) //auto_increment 사용시 알아서 값 증가됨.
     private Long id;
+    @NotNull
+    @Size(min=2, max=30, message = "제목은 2자이상 30자 이하입니다.")
     private String title;
     private String content;
 }
